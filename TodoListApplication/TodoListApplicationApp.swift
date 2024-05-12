c25ed1b6347cf19353034b7c2e500c7c9247556c
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-12 12:52:26 -0700

    Initial Commit

---
+++ TodoListApplication/TodoListApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApplicationApp.swift
+//  TodoListApplication
+//
+//  Created by Ricky Villareal on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
